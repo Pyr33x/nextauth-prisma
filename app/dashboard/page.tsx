14cf71f44e8946dd4e9fd37676f97f84215ab3f6
Mehdi <woahmehdi@gmail.com>
2024-08-06 16:16:17 +0330

    chore: changed user color state

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,7 +8,7 @@ export default async function Dashboard() {
     <div className="mx-auto max-w-2xl px-4 md:px-0">
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-center text-4xl font-black text-white md:text-6xl">
-          Welcome <span className="text-violet-800">{session?.user?.name}</span>
+          Welcome <span className="text-blue-600">{session?.user?.name}</span>
           👋
         </h1>
         <p className="mt-2 text-balance text-center text-xl font-medium text-neutral-600 md:text-2xl">
